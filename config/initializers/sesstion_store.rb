92ac7935eb7bf9972adc8b8690025b0a6de9e21d
daiki-shiroma <wli34012@gmail.com>
2023-03-02 15:09:23 +0900

    add_session

--- config/initializers/sesstion_store.rb
+++ config/initializers/sesstion_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-    Rails.application.config.session_store :cookie_store, key: '_auth-app-api', domain: 'フロントエンドのドメイン'
+    Rails.application.config.session_store :cookie_store, key: '_auth-app-api', domain: 'rails-api-auth.fly.dev'
 else
     Rails.application.config.session_store :cookie_store, key: '_auth-app-api'
 end
